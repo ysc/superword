14dd055b2d28d6c2b489a484cb8e9fa12ccc4d0d
ysc <yang-shangchuan@qq.com>
2015-03-02 06:21:46 +0800

    字符映射

---
+++ src/main/java/org/apdplat/superword/model/CharMap.java
@@ -0,0 +1,52 @@
+/**
+ *
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.apdplat.superword.model;
+
+/**
+ * 字符映射
+ * @author 杨尚川
+ */
+public class CharMap{
+    private String from;
+    private String to;
+
+    public CharMap(){}
+    public CharMap(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+}
