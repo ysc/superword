635570bf99346ddeeb7674e6bdbf5323e4054a7e
ysc <yang-shangchuan@qq.com>
2015-03-22 04:42:05 +0800

    规则改进

--- src/main/java/org/apdplat/superword/rule/PrefixRule.java
+++ src/main/java/org/apdplat/superword/rule/PrefixRule.java
@@ -25,9 +25,9 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+import org.apdplat.superword.model.ComplexPrefix;
 import org.apdplat.superword.model.Prefix;
 import org.apdplat.superword.model.Word;
-import org.apdplat.superword.tools.PrefixExtractor;
 import org.apdplat.superword.tools.WordLinker;
 import org.apdplat.superword.tools.WordSources;
 
@@ -38,29 +38,35 @@ import org.apdplat.superword.tools.WordSources;
 public class PrefixRule {
     private PrefixRule(){}
 
-    public static TreeMap<Prefix, List<Word>> findByPrefix(Collection<Word> words, Collection<Prefix> prefixes) {
+    public static TreeMap<Prefix, List<Word>> findByPrefix(Collection<Word> words, Collection<Prefix> prefixes, boolean strict) {
         TreeMap<Prefix, List<Word>> map = new TreeMap<>();
         for(Prefix prefix : prefixes){
-            map.put(prefix, findByPrefix(words, prefix));
+            map.put(prefix, findByPrefix(words, prefix, strict));
         }
         return map;
     }
 
-    public static List<Word> findByPrefix(Collection<Word> words, Prefix prefix) {
+    public static List<Word> findByPrefix(Collection<Word> words, Prefix prefix, boolean strict) {
         return words
                 .parallelStream()
                 .filter(word -> {
                     String w = word.getWord();
-                    boolean hit = false;
-                    String[] ps = prefix.getPrefix().toLowerCase().split(",");
-                    for (String p : ps) {
-                        p = p.replaceAll("-", "").replaceAll("\\s+", "");
-                        if (w.toLowerCase().startsWith(p)) {
-                            hit = true;
-                            break;
-                        }
+                    if(Character.isUpperCase(w.charAt(0))){
+                        return false;
                     }
-                    return hit;
+                    String p = prefix.getPrefix().toLowerCase();
+
+                    if(strict){
+                        if(w.startsWith(p)
+                                && w.length()-p.length()>2
+                                && words.contains(new Word(w.substring(p.length()), ""))){
+                            return true;
+                        }
+                    } else if (w.startsWith(p)) {
+                        return true;
+                    }
+
+                    return false;
                 })
                 .sorted()
                 .collect(Collectors.toList());
@@ -82,29 +88,47 @@ public class PrefixRule {
                     .append(words.size())
                     .append(")</h2></br>\n");
             AtomicInteger wordCounter = new AtomicInteger();
+            html.append("<table>\n");
             words.forEach(word -> {
-                html.append("\t")
-                        .append(wordCounter.incrementAndGet())
-                        .append("、")
-                        .append(WordLinker.toLink(word.getWord(), prefix.getPrefix()))
+                if(wordCounter.get()%3 == 0){
+                    if(wordCounter.get() == 0){
+                        html.append("\t<tr>");
+                    }else{
+                        html.append("</tr>\n\t<tr>");
+                    }
+                }
+                wordCounter.incrementAndGet();
+                html.append("<td>")
+                        .append(WordLinker.toLink(word.getWord(), prefix.getPrefix(), "<b>", "</b>-"))
                         .append("    ")
                         .append(WordLinker.toLink(word.getWord().substring(prefix.getPrefix().length())))
-                        .append("</br>\n");
+                        .append("</td>");
             });
+            if(html.toString().endsWith("<tr>")){
+                html.setLength(html.length()-5);
+            }else{
+                html.append("</tr>\n");
+            }
+            html.append("</table>\n");
         }
         return html.toString();
     }
 
     public static void main(String[] args) throws Exception {
-        Set<Word> words = WordSources.get("/words.txt", "/words_extra.txt", "/words_gre.txt", "/words_anc.txt");
+        Set<Word> words = WordSources.get("/words.txt",
+                                        "/words_extra.txt",
+                                        //"/words_anc.txt",
+                                        "/words_gre.txt"
+                                        );
         //List<Prefix> prefixes = PrefixExtractor.extract();
         //List<Prefix> prefixes = Arrays.asList(new Prefix("mono,mon", "单个，一个"));
         //List<Prefix> prefixes = Arrays.asList(new Prefix("antiq", "=old,表示\"古老\""));
         //List<Prefix> prefixes = Arrays.asList(new Prefix("pseud", "=fake,表示\"假的\""));
         //List<Prefix> prefixes = Arrays.asList(new Prefix("super", "表示\"在……上面\"或表示\"超级,超过,过度\""));
-        List<Prefix> prefixes = Arrays.asList(new Prefix("semi", "表示\"在……上面\"或表示\"超级,超过,过度\""));
+        //List<Prefix> prefixes = Arrays.asList(new Prefix("semi", "表示\"在……上面\"或表示\"超级,超过,过度\""));
+        List<Prefix> prefixes = new ComplexPrefix("dis-,in-,im-,il-,ir-,un-,mis-,non-,dis-,de-,anti-,counter-", "否定前缀").simplify();
 
-        TreeMap<Prefix, List<Word>> prefixToWords = PrefixRule.findByPrefix(words, prefixes);
+        TreeMap<Prefix, List<Word>> prefixToWords = PrefixRule.findByPrefix(words, prefixes, true);
         String htmlFragment = PrefixRule.toHtmlFragment(prefixToWords);
 
         Files.write(Paths.get("target/prefix_rule.txt"),htmlFragment.getBytes("utf-8"));
