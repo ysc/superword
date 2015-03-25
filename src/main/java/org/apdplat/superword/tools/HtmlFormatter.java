8c28b789df4c3947db9f7344288483661c16c73c
ysc <yang-shangchuan@qq.com>
2015-03-26 06:35:27 +0800

    重构代码

--- src/main/java/org/apdplat/superword/tools/HtmlFormatter.java
+++ src/main/java/org/apdplat/superword/tools/HtmlFormatter.java
@@ -19,13 +19,13 @@
  */
 package org.apdplat.superword.tools;
 
-import org.apdplat.superword.model.Prefix;
 import org.apdplat.superword.model.Word;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * HTML格式化工具，将生成的HTML片段发布到网络上的博客、日志中
@@ -38,30 +38,42 @@ public class HtmlFormatter {
         return toHtmlTableFragment(words.entrySet(), rowLength);
     }
     public static String toHtmlTableFragment(Set<Map.Entry<Word, AtomicInteger>> words, int rowLength) {
+
+        List<String> data =
+        words
+            .stream()
+            .sorted((a, b) -> b.getValue().get() - a.getValue().get())
+            .map(entry -> {
+                String link = WordLinker.toLink(entry.getKey().getWord());
+                if (entry.getValue().get() > 0) {
+                    link = link+"-"+entry.getValue().get();
+                }
+                return link;
+            })
+            .collect(Collectors.toList());
+
+        return toHtmlTableFragment(data, rowLength);
+    }
+
+    public static String toHtmlTableFragment(List<String> data, int rowLength) {
         StringBuilder html = new StringBuilder();
 
         AtomicInteger rowCounter = new AtomicInteger();
         AtomicInteger wordCounter = new AtomicInteger();
         html.append("<table  border=\"1\">\n");
-        words
-            .stream()
-            .sorted((a, b) -> b.getValue().get() - a.getValue().get())
-            .forEach(entry -> {
+        data
+            .forEach(datum -> {
                 if (wordCounter.get() % rowLength == 0) {
                     if (wordCounter.get() == 0) {
-                        rowCounter.incrementAndGet();
-                        html.append("\t<tr><td>").append(rowCounter.get()).append("</td>");
+                        html.append("\t<tr>");
                     } else {
-                        rowCounter.incrementAndGet();
-                        html.append("</tr>\n\t<tr><td>").append(rowCounter.get()).append("</td>");
+                        html.append("</tr>\n\t<tr>");
                     }
+                    rowCounter.incrementAndGet();
+                    html.append("<td>").append(rowCounter.get()).append("</td>");
                 }
                 wordCounter.incrementAndGet();
-                html.append("<td>")
-                    .append(WordLinker.toLink(entry.getKey().getWord()))
-                    .append("-")
-                    .append(entry.getValue().get())
-                    .append("</td>");
+                html.append("<td>").append(datum).append("</td>");
             });
         if(html.toString().endsWith("<tr>")){
             html.setLength(html.length()-5);
