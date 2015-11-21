592a5f7fd0fd85168e4459702abcbe78b1a1bff7
ysc <yang-shangchuan@qq.com>
2015-11-21 15:32:12 +0800

    方法公开

--- src/main/java/org/apdplat/superword/rule/SuffixRule.java
+++ src/main/java/org/apdplat/superword/rule/SuffixRule.java
@@ -104,7 +104,7 @@ public class SuffixRule{
                 .collect(Collectors.toList());
     }
 
-    private static Map<Word, List<Word>> convert(Map<Suffix, List<Word>> data){
+    public static Map<Word, List<Word>> convert(Map<Suffix, List<Word>> data){
         Map<Word, List<Word>> r = new HashMap<>();
         data.keySet().forEach(k -> r.put(new Word(k.getSuffix(), k.getDes()), data.get(k)));
         return r;
