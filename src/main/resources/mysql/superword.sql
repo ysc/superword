797d295d09f3954a41e0d8e9880179dbf1d46365
ysc <yang-shangchuan@qq.com>
2015-11-25 12:49:52 +0800

    MySQL数据库脚本

---
+++ src/main/resources/mysql/superword.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE superword;
+
+USE superword;
+
+CREATE  TABLE `superword`.`user_word` (
+
+  `id` INT NOT NULL AUTO_INCREMENT ,
+
+  `user_name` VARCHAR(200)  NOT NULL ,
+
+  `word` VARCHAR(200)  NOT NULL ,
+
+  `dictionary` VARCHAR(200)  NOT NULL ,
+
+  `date_time` TIMESTAMP  NOT NULL ,
+
+  PRIMARY KEY (`id`) ,
+
+  INDEX `user_name_INDEX` (`user_name` ASC) ,
+
+  INDEX `date_time_INDEX` (`date_time` ASC) );
