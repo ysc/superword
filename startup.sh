0abb59abba441a41580bcfd0a464e3864a12b98c
ysc <yang-shangchuan@qq.com>
2015-11-27 00:31:50 +0800

    运行脚本

--- startup.sh
+++ startup.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
-if [ -f ~/tomcat-8.0.28.zip ] ; then
-    echo tomcat-8.0.28 prepared
+if [ -f ~/tomcat-8.0.29.zip ] ; then
+    echo tomcat-8.0.29 prepared
+    pid=$(jps | grep Bootstrap | awk -F ' ' '{print $1}')
+    kill -9 $pid
+    echo tomcat stoped
 else
-    echo downloading tomcat8.0.21...
-    wget http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.28/bin/apache-tomcat-8.0.28.zip
-    echo tomcat has been downloaded
-    mv apache-tomcat-8.0.28.zip ~/tomcat-8.0.28.zip
+    echo downloading tomcat8.0.29...
+    wget http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.29/bin/apache-tomcat-8.0.29.zip
+    echo tomcat downloaded
+    mv apache-tomcat-8.0.29.zip ~/tomcat-8.0.29.zip
     echo copy finished
-    unzip -d ~/tomcat-8.0.28 ~/tomcat-8.0.28.zip
+    unzip -d ~/tomcat-8.0.29 ~/tomcat-8.0.29.zip
     echo unzip finished
+    chmod +x ~/tomcat-8.0.29/apache-tomcat-8.0.29/bin/*.sh
+    mkdir ~/tomcat-8.0.29/apache-tomcat-8.0.29/conf/Catalina/
+    mkdir ~/tomcat-8.0.29/apache-tomcat-8.0.29/conf/Catalina/localhost/
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > ~/tomcat-8.0.29/apache-tomcat-8.0.29/conf/Catalina/localhost/superword.xml
+    echo "<Context antiJARLocking=\"true\" docBase=\"$(pwd)/target/superword-1.0\" path=\"/superword\"/>" >> ~/tomcat-8.0.29/apache-tomcat-8.0.29/conf/Catalina/localhost/superword.xml
 fi
-mvn clean install
-echo clean install finished
-rm -rf ~/tomcat-8.0.28/apache-tomcat-8.0.28/webapps/superword-1.0
-echo old webapps directory deleted
-cp target/superword-1.0.war ~/tomcat-8.0.28/apache-tomcat-8.0.28/webapps/
-echo copy war finished
-rm -rf ~/tomcat-8.0.28/apache-tomcat-8.0.28/logs
+mvn package
+echo package finished
+rm -rf ~/tomcat-8.0.29/apache-tomcat-8.0.29/logs
 echo old logs directory deleted
-mkdir ~/tomcat-8.0.28/apache-tomcat-8.0.28/logs
+mkdir ~/tomcat-8.0.29/apache-tomcat-8.0.29/logs
 echo mkdir logs directory
-chmod +x ~/tomcat-8.0.28/apache-tomcat-8.0.28/bin/*.sh
-~/tomcat-8.0.28/apache-tomcat-8.0.28/bin/catalina.sh start
-echo apdplat has been started
-tail -f ~/tomcat-8.0.28/apache-tomcat-8.0.28/logs/*
+~/tomcat-8.0.29/apache-tomcat-8.0.29/bin/catalina.sh start
+echo superword started
+tail -f ~/tomcat-8.0.29/apache-tomcat-8.0.29/logs/*
