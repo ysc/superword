a73d44e3ea25afa3b50f364f31687f6202ffe622
ysc <yang-shangchuan@qq.com>
2015-11-28 00:25:06 +0800

    用户书籍分析记录

---
+++ src/main/java/org/apdplat/superword/model/UserBook.java
@@ -0,0 +1,81 @@
+/*
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.apdplat.superword.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 用户书籍分析记录
+ * Created by ysc on 11/24/15.
+ */
+public class UserBook {
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private int id;
+    private String userName;
+    private String book;
+    private Date dateTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public String getDateTimeString(){
+        return simpleDateFormat.format(dateTime);
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "UserWord{" +
+                "id='" + id + '\'' +
+                "userName='" + userName + '\'' +
+                ", dateTime=" + simpleDateFormat.format(dateTime) +
+                ", book='" + book + '\'' +
+                '}';
+    }
+}
