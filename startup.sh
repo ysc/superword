5fc07013b0d9d14e0af54bf7aeb261729c7f2bb2
ysc <yang-shangchuan@qq.com>
2015-11-27 22:02:56 +0800

    系统运行脚本

--- startup.sh
+++ startup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-if [ -f ~/tomcat-8.0.29.zip ] ; then
+if [ -f pom.xml ] ; then
+    echo superword source code prepared
+else
+    wget https://github.com/ysc/superword/archive/master.zip
+    echo superword source code downloaded
+    unzip master.zip
+    cd superword-master
+    echo unzip finished
+fi
+if [ -f ~/apache-tomcat-8.0.29.zip ] ; then
     echo tomcat-8.0.29 prepared
     pid=$(jps | grep Bootstrap | awk -F ' ' '{print $1}')
     kill -9 $pid
@@ -8,22 +17,29 @@ else
     echo downloading tomcat8.0.29...
     wget http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.29/bin/apache-tomcat-8.0.29.zip
     echo tomcat downloaded
-    mv apache-tomcat-8.0.29.zip ~/tomcat-8.0.29.zip
+    mv apache-tomcat-8.0.29.zip ~/apache-tomcat-8.0.29.zip
     echo copy finished
-    unzip -d ~/tomcat-8.0.29 ~/tomcat-8.0.29.zip
+    unzip -d ~/ ~/apache-tomcat-8.0.29.zip
     echo unzip finished
-    chmod +x ~/tomcat-8.0.29/apache-tomcat-8.0.29/bin/*.sh
-    mkdir ~/tomcat-8.0.29/apache-tomcat-8.0.29/conf/Catalina/
-    mkdir ~/tomcat-8.0.29/apache-tomcat-8.0.29/conf/Catalina/localhost/
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > ~/tomcat-8.0.29/apache-tomcat-8.0.29/conf/Catalina/localhost/superword.xml
-    echo "<Context antiJARLocking=\"true\" docBase=\"$(pwd)/target/superword-1.0\" path=\"/superword\"/>" >> ~/tomcat-8.0.29/apache-tomcat-8.0.29/conf/Catalina/localhost/superword.xml
+    chmod +x ~/apache-tomcat-8.0.29/bin/*.sh
+    mkdir ~/apache-tomcat-8.0.29/conf/Catalina/
+    mkdir ~/apache-tomcat-8.0.29/conf/Catalina/localhost/
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > ~/apache-tomcat-8.0.29/conf/Catalina/localhost/superword.xml
+    echo "<Context antiJARLocking=\"true\" docBase=\"$(pwd)/target/superword-1.0\" path=\"/superword\"/>" >> ~/apache-tomcat-8.0.29/conf/Catalina/localhost/superword.xml
+    wget http://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+    echo maven downloaded
+    mv apache-maven-3.3.9-bin.tar.gz ~/apache-maven-3.3.9.tar.gz
+    echo copy finished
+    tar -xzvf ~/apache-maven-3.3.9.tar.gz -C ~/
+    echo unzip finished
+    chmod +x ~/apache-maven-3.3.9/bin/*
 fi
-mvn package
+~/apache-maven-3.3.9/bin/mvn package
 echo package finished
-rm -rf ~/tomcat-8.0.29/apache-tomcat-8.0.29/logs
+rm -rf ~/apache-tomcat-8.0.29/logs
 echo old logs directory deleted
-mkdir ~/tomcat-8.0.29/apache-tomcat-8.0.29/logs
+mkdir ~/apache-tomcat-8.0.29/logs
 echo mkdir logs directory
-~/tomcat-8.0.29/apache-tomcat-8.0.29/bin/catalina.sh start
+~/apache-tomcat-8.0.29/bin/catalina.sh start
 echo superword started
-tail -f ~/tomcat-8.0.29/apache-tomcat-8.0.29/logs/*
+tail -f ~/apache-tomcat-8.0.29/logs/*
