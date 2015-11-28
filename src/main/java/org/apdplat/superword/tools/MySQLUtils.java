462baca5d9133caf4e6f4bc9357be36dc6159e87
ysc <yang-shangchuan@qq.com>
2015-11-28 19:06:26 +0800

    用户注册认证数据存储服务

--- src/main/java/org/apdplat/superword/tools/MySQLUtils.java
+++ src/main/java/org/apdplat/superword/tools/MySQLUtils.java
@@ -19,10 +19,8 @@
 package org.apdplat.superword.tools;
 
 
-import org.apdplat.superword.model.UserBook;
-import org.apdplat.superword.model.UserText;
-import org.apdplat.superword.model.UserUrl;
-import org.apdplat.superword.model.UserWord;
+import org.apache.commons.lang.StringUtils;
+import org.apdplat.superword.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,7 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 追踪用户查词记录
+ * 数据存储层
  * @author 杨尚川
  */
 public class MySQLUtils {
@@ -51,6 +49,55 @@ public class MySQLUtils {
     }
 
     private MySQLUtils() {
+    }
+
+    public static boolean login(User user){
+        String sql = "select password from user where user_name=?";
+        Connection con = getConnection();
+        if(con == null){
+            return false;
+        }
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setString(1, user.getUserName());
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                String password = rs.getString(1);
+                if(StringUtils.isNotBlank(user.getPassword()) && user.getPassword().equals(password)){
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("登录失败", e);
+        } finally {
+            close(con, pst, rs);
+        }
+        return false;
+    }
+
+    public static boolean register(User user) {
+        String sql = "insert into user (user_name, password, date_time) values (?, ?, ?)";
+        Connection con = getConnection();
+        if(con == null){
+            return false;
+        }
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setString(1, user.getUserName());
+            pst.setString(2, user.getPassword());
+            pst.setTimestamp(3, new Timestamp(user.getDateTime().getTime()));
+            pst.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            LOG.error("注册失败", e);
+        } finally {
+            close(con, pst, rs);
+        }
+        return false;
     }
 
     public static UserText getUseTextFromDatabase(int id) {
