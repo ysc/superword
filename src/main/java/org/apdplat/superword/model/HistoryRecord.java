8e83d53d10feb1099b3c583edd29bbee2fb9c292
ysc <yang-shangchuan@qq.com>
2015-11-29 18:31:02 +0800

    用户历史记录抽象基类

---
+++ src/main/java/org/apdplat/superword/model/HistoryRecord.java
@@ -0,0 +1,62 @@
+/*
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.apdplat.superword.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 用户历史记录抽象基类
+ * Created by ysc on 11/29/15.
+ */
+public class HistoryRecord {
+    protected static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    protected int id;
+    protected String userName;
+    protected Date dateTime;
+
+    public String getDateTimeString(){
+        return simpleDateFormat.format(dateTime);
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
