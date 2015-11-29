a0f3209adaf2aaa9b33f36ac1101a701bf02f184
ysc <yang-shangchuan@qq.com>
2015-11-29 18:34:14 +0800

    用户动态后缀分析记录

---
+++ src/main/java/org/apdplat/superword/model/UserDynamicSuffix.java
@@ -0,0 +1,45 @@
+/*
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.apdplat.superword.model;
+
+/**
+ * 用户动态后缀分析记录
+ * Created by ysc on 11/24/15.
+ */
+public class UserDynamicSuffix extends HistoryRecord {
+    private String dynamicSuffix;
+
+    public String getDynamicSuffix() {
+        return dynamicSuffix;
+    }
+
+    public void setDynamicSuffix(String dynamicSuffix) {
+        this.dynamicSuffix = dynamicSuffix;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDynamicSuffix{" +
+                "id='" + id + '\'' +
+                "userName='" + userName + '\'' +
+                ", dateTime=" + simpleDateFormat.format(dateTime) +
+                ", dynamicSuffix='" + dynamicSuffix + '\'' +
+                '}';
+    }
+}
