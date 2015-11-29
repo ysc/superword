1a662932b5e48bd638cbb998012d523712e474c0
ysc <yang-shangchuan@qq.com>
2015-11-29 18:40:44 +0800

    用户相似词查询记录数据库脚本

--- src/main/resources/mysql/superword.sql
+++ src/main/resources/mysql/superword.sql
@@ -125,3 +125,21 @@ CREATE  TABLE `superword`.`user_dynamic_prefix` (
   INDEX `user_name_INDEX` (`user_name` ASC) ,
 
   INDEX `date_time_INDEX` (`date_time` ASC) );
+
+CREATE  TABLE `superword`.`user_similar_word` (
+
+  `id` INT NOT NULL AUTO_INCREMENT ,
+
+  `user_name` VARCHAR(200)  NOT NULL,
+
+  `similar_word` VARCHAR(32)  NOT NULL,
+
+  `md5` VARCHAR(32)  NOT NULL UNIQUE,
+
+  `date_time` TIMESTAMP  NOT NULL ,
+
+  PRIMARY KEY (`id`) ,
+
+  INDEX `user_name_INDEX` (`user_name` ASC) ,
+
+  INDEX `date_time_INDEX` (`date_time` ASC) );
