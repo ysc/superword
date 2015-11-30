ea3649fdb7271870507103b1ffa601bb6f691406
ysc <yang-shangchuan@qq.com>
2015-11-30 23:06:28 +0800

    QQ登陆数据库脚本

--- src/main/resources/mysql/superword.sql
+++ src/main/resources/mysql/superword.sql
@@ -78,7 +78,7 @@ CREATE  TABLE `superword`.`user` (
 
   `id` INT NOT NULL AUTO_INCREMENT ,
 
-  `user_name` VARCHAR(20)  NOT NULL UNIQUE,
+  `user_name` VARCHAR(35)  NOT NULL UNIQUE,
 
   `password` VARCHAR(32)  NOT NULL,
 
@@ -135,6 +135,36 @@ CREATE  TABLE `superword`.`user_similar_word` (
   `similar_word` VARCHAR(32)  NOT NULL,
 
   `md5` VARCHAR(32)  NOT NULL UNIQUE,
+
+  `date_time` TIMESTAMP  NOT NULL ,
+
+  PRIMARY KEY (`id`) ,
+
+  INDEX `user_name_INDEX` (`user_name` ASC) ,
+
+  INDEX `date_time_INDEX` (`date_time` ASC) );
+
+CREATE  TABLE `superword`.`user_qq` (
+
+  `id` INT NOT NULL AUTO_INCREMENT ,
+
+  `user_name` VARCHAR(35)  NOT NULL UNIQUE,
+
+  `password` VARCHAR(32)  NOT NULL,
+
+  `nickname` VARCHAR(32)  NOT NULL,
+
+  `gender` VARCHAR(5)  NOT NULL,
+
+  `birthday` VARCHAR(10)  NOT NULL,
+
+  `location` VARCHAR(50)  NOT NULL,
+
+  `avatarURL30` VARCHAR(150)  NOT NULL,
+
+  `avatarURL50` VARCHAR(150)  NOT NULL,
+
+  `avatarURL100` VARCHAR(150)  NOT NULL,
 
   `date_time` TIMESTAMP  NOT NULL ,
 
