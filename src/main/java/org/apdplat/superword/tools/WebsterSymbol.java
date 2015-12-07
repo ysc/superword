44dd1eaabeb4118f65ab3fe1d33d7d8212cfea02
ysc <yang-shangchuan@qq.com>
2015-12-07 10:41:24 +0800

    韦氏词典音标符号列表

---
+++ src/main/java/org/apdplat/superword/tools/WebsterSymbol.java
@@ -0,0 +1,28 @@
+/*
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.apdplat.superword.tools;
+
+/**
+ * 韦氏词典音标符号列表
+ * Created by ysc on 12/7/15.
+ */
+public enum WebsterSymbol {
+    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, r, s, t, u, v, w, y, z,
+    ä, ü, ā, ē, ī, ŋ, ō, ə;
+}
