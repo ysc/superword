db9037d41c7aed85056e9c00c7e0e445c06cb540
ysc <yang-shangchuan@qq.com>
2015-12-07 10:41:10 +0800

    牛津词典音标符号列表

---
+++ src/main/java/org/apdplat/superword/tools/OxfordSymbol.java
@@ -0,0 +1,28 @@
+/*
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.apdplat.superword.tools;
+
+/**
+ * 牛津词典音标符号列表
+ * Created by ysc on 12/7/15.
+ */
+public enum OxfordSymbol {
+    a, b, d, e, f, ɡ, h, i, j, k, l, m, n, o, p, r, s, t, u, v, w, z,
+    ð, ŋ, ɑ, ɒ, ɔ, ə, ɛ, ɜ, ɪ, ʃ, ʊ, ʌ, ʒ, θ;
+}
