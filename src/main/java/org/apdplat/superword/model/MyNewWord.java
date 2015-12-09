c5af9ddeabe92fe1103e491d72321c374fc980d7
ysc <yang-shangchuan@qq.com>
2015-12-09 18:10:10 +0800

    我的生词

---
+++ src/main/java/org/apdplat/superword/model/MyNewWord.java
@@ -0,0 +1,44 @@
+/*
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.apdplat.superword.model;
+
+/**
+ * 我的生词
+ * Created by ysc on 11/24/15.
+ */
+public class MyNewWord extends HistoryRecord {
+    private String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public String toString() {
+        return "MyNewWord{" +
+                "userName='" + userName + '\'' +
+                ", word='" + word + '\'' +
+                ", dateTime=" + simpleDateFormat.format(dateTime) +
+                '}';
+    }
+}
