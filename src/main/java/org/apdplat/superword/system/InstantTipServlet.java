a2c9cf96fdc992ce012d328c8a85a6774a3cde16
ysc <yang-shangchuan@qq.com>
2015-12-11 18:11:42 +0800

    word input autocomplete

---
+++ src/main/java/org/apdplat/superword/system/InstantTipServlet.java
@@ -0,0 +1,51 @@
+/*
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.apdplat.superword.system;
+
+import org.apache.commons.lang.StringUtils;
+import org.apdplat.word.recognition.RecognitionTool;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * word input autocomplete
+ * Created by ysc on 12/11/15.
+ */
+public class InstantTipServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        String prefix = request.getParameter("prefix");
+        if(StringUtils.isNotBlank(prefix)
+                && RecognitionTool.isEnglish(prefix)){
+            response.getWriter().write(InstantTip.getWordsByPrefix(prefix));
+        }else {
+            response.getWriter().write("");
+        }
+    }
+}
